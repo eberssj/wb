e9319bf74145ad6f89dc10f9a823da4ef91d03d8
Eber Junior <eberssjunior12@gmail.com>
2024-06-18 16:21:21 -0300

    lista 2

--- atvi-wb-typescript/dist/modelo/produto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Produto {
-    constructor(nome, valor) {
-        this.nome = nome;
-        this.valor = valor;
-    }
-}
-exports.default = Produto;
